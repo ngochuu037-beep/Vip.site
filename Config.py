6e1fec1699301cbd1be972dd48feba33622b6ac5
ngochuu037-beep <ngochuu037@gmail.com>
2026-07-05 16:05:56 +0700

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,25 @@
+# config.py
+import os
+import base64
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key-change-in-production")
+    FIREBASE_URL = "https://tool-free-fire-by-default-rtdb.asia-southeast1.firebasedatabase.app"
+    FIREBASE_SECRET = ""  # Nếu cần auth
+
+    # Các hằng số mã hóa
+    AES_KEY = bytes([89,103,38,116,99,37,68,69,117,104,54,37,90,99,94,56])
+    AES_IV  = bytes([54,111,121,90,68,114,50,50,69,51,121,99,104,106,77,37])
+    SECRET_KEY_BYTES = b"1e5898ccb8dfdd921f9bdea848768b64a201"
+
+    # Admin (có thể đọc từ biến môi trường)
+    ADMIN_USERNAME = "chip"
+    ADMIN_PASSWORD_HASH = hashlib.sha256("moimoimoi111".encode()).hexdigest()
+
+    # File cache spam log
+    SPAM_CACHE_FILE = 'active_spams_cache.json'
+
+    BAN7_BODY_BASE64 = (
+        'vGkQhkkYHjne06dPbmJgb36BQ1NdLgk8J+uc+z4/9t4OZ19iWMyn5cH/Pe/DgGHrwHxJ+dRKGho2LCErl+rBWEf/6aWcFflRXiEsvPiGKM3809a+vci8mAQBREdizRWQ6bdeLnlztsqBvlB5OU8WFlmGxsU8UY1U3Zp/eLNTbq0DHqjOxziR+ylXgLlonsckeKvaxa4YE540eXi+9v4ilJunUubievpqUip6XDAyKV7o1spVxiaP0z4d8MLosbeYthPAnK5ykeE8IpnYaru0oDN8o90r820h04frRPJBszlDiarwdjgXaiyeQqAiOgEN63gUoVq2rd0JfYGaHN2f2kJxxO9uCYxyJ6IhCzQq8yAJT2asKa9u7gWB1bB/fJxq4nVxY8am8DI+rqIDvVSF3EdQBDh9qipPFCd0gZx7kDVg/9vM79YAE+FnDgGY3D/niKWsu66SL9+bRcghZxcCMOzKwvRe7hCRU2pDjBw0MRvPnCCa9KpEuO4CgWz+++SP9whlI0dWCi9/snDCN6i9V2TYrSWfbg1i2TRipquGUoi/cP1xPBeMwQlzlf4APMQzvT8MOQotqry+y1+koTpwRKlWgu7QLmiumn4dwd9HARVMThSH46kwlD8xep4sLVf6/BbjWixBMVRKFi1w9zpVVe+w6rBYhtBHXfjqjg2sCzF1mlBabMbW4L2yXEmABaQG/l0jmaGEWh6kzMY9T1nzV1Wcw5lF7X+pwQEnAn6i5coowNGKrTGUJ2wa3+tAxGcm9zozCvj8yd2pOXmta46GoREDQk+U99uHHvjqzsSNeBq8ffL5zibtv0pZPhnUuSP76YkhCcdtDilaecBElnt9eFfo8cy2B3Z0wbhG20nKNfYuhgZMZuSPRjmQphlfyl1hpoSG5xMQ7bdqZAkoTkZlFpCL4y02yUlImI7Z8jnA3i4un3UOq1rXrMza+bqNsMhrJ/aUS3mnoXr23yzuUc56zyYQtzJx6VCupsHraP7brcDbBS76Gp2o0oT2iE4Y55ZyAEgdt307DzJknHEHdGuoOG4Yzy5bI7HnukmnUjoiIdJEr7iJdOLppdB+ZDXPkHps5ysskdapRp0i2x1gMpW9XU1LY1cNAsTmAvHcz2GZA2OjtvS0roiay2rkUqNgmN8cPygK3j6ycfpkHc1PkUnmG1CNjMy3qP7c18qvDdSYfiq99Wra4l5L2dV3dE/kGpc1fgwWo94UPIes67wg/TrRR85GxPcpIX3IUOGMyEX1VWJTS2PvTm3S4xrerobDKG5V'
+    )
+    BAN7_API_URL = 'https://clientbp.ggpolarbear.com/GetLoginData'
